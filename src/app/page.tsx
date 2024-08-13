588eac835c143bd57fe4f626de23863073535342
Yatsyshyn Oleksandr <pandikgenshin@gmail.com>
2024-08-13 21:11:39 +0300

    refactor: change logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import { GrHelpBook } from 'react-icons/gr'
 
 import { Search } from '@/components/Search'
 import { ThemeSwitcher } from '@/components/ThemeSwitcher'
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="mx-auto flex max-w-5xl flex-col gap-10 p-6">
       <header className="flex w-full justify-between">
-        <Image src={'./logo.svg'} width={32} height={32} alt="logo" />
+        <GrHelpBook size={32} className="text-primary" />
         <ThemeSwitcher />
       </header>
       <Search />
