04bf95b1e591398a109e58f490d8ebb2f31f7c1a
Yatsyshyn Oleksandr <pandikgenshin@gmail.com>
2024-08-15 22:06:20 +0300

    refactor(optimize project structure): add main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 'use client'
 
 import { useState } from 'react'
-import Link from 'next/link'
 import { useQuery } from '@tanstack/react-query'
-import { FaExternalLinkAlt } from 'react-icons/fa'
 import { GrHelpBook } from 'react-icons/gr'
-import { IoIosPause, IoIosPlay } from 'react-icons/io'
 
 import { Error } from '@/components/Error'
 import { Instructions } from '@/components/Instructions'
-import { Meaning } from '@/components/Meaning'
+import { Loading } from '@/components/Loading'
+import { Main } from '@/components/Main'
 import { Search } from '@/components/Search'
 import { ThemeSwitcher } from '@/components/ThemeSwitcher'
 
 import WordData, { ErrorResponse } from './type'
-import { Loading } from "@/components/Loading"
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('')
@@ -104,56 +101,11 @@ export default function Home() {
         ) : error ? (
           <Error />
         ) : data ? (
-          <>
-            <section className="mb-5 flex items-center justify-between">
-              <div>
-                <h1 className="text-fs-title capitalize">{data.word}</h1>
-                <p className="text-fs-sub-title tracking-widest text-primary">
-                  {data.phonetic}
-                </p>
-              </div>
-              <button
-                onClick={speechHandler}
-                className="flex h-12 w-12 items-center justify-center rounded-full bg-accent md:h-16 md:w-16"
-              >
-                {isPlaying ? (
-                  <IoIosPause
-                    className="text-3xl text-primary md:text-5xl"
-                    id="play-icon"
-                  />
-                ) : (
-                  <IoIosPlay
-                    className="ml-1 text-3xl text-primary md:text-5xl"
-                    id="play-icon"
-                  />
-                )}
-              </button>
-            </section>
-            {data?.meanings.map((meaning, index) => (
-              <Meaning
-                key={index}
-                antonyms={meaning.antonyms}
-                definitions={meaning.definitions}
-                partOfSpeech={meaning.partOfSpeech}
-                synonyms={meaning.synonyms}
-              />
-            ))}
-            {data?.sourceUrls && data?.sourceUrls.length > 0 && (
-              <div className="mt-6 border-t border-gray-200 py-5">
-                <div className="flex flex-wrap items-center">
-                  <p className="text-xl">Source</p>
-                  <Link
-                    target="_blank"
-                    href={data.sourceUrls[0]}
-                    className="flex flex-wrap items-center text-fs-link text-gray-400 underline"
-                  >
-                    <span> {data.sourceUrls}</span>
-                    <FaExternalLinkAlt className="text-sm" />
-                  </Link>
-                </div>
-              </div>
-            )}
-          </>
+          <Main
+            data={data}
+            isPlaying={isPlaying}
+            speechHandler={speechHandler}
+          />
         ) : (
           <Instructions />
         )}
---
+++ src/components/Main.tsx
@@ -0,0 +1,66 @@
+import WordData from "@/app/type"
+import Link from "next/link"
+import { FaExternalLinkAlt } from "react-icons/fa"
+import { Meaning } from "./Meaning"
+import { IoIosPause, IoIosPlay } from "react-icons/io"
+
+type Props = {
+	data: WordData
+  isPlaying: boolean
+  speechHandler: () => void
+}
+
+export const Main = ({ data, isPlaying, speechHandler }: Props) => {
+	return (
+		<>
+      <section className="mb-5 flex items-center justify-between">
+        <div>
+          <h1 className="text-fs-title capitalize">{data.word}</h1>
+          <p className="text-fs-sub-title tracking-widest text-primary">
+            {data.phonetic}
+          </p>
+        </div>
+        <button
+          onClick={speechHandler}
+          className="flex h-12 w-12 items-center justify-center rounded-full bg-accent md:h-16 md:w-16"
+        >
+          {isPlaying ? (
+            <IoIosPause
+              className="text-3xl text-primary md:text-5xl"
+              id="play-icon"
+            />
+          ) : (
+            <IoIosPlay
+              className="ml-1 text-3xl text-primary md:text-5xl"
+              id="play-icon"
+            />
+          )}
+        </button>
+      </section>
+      {data?.meanings.map((meaning, index) => (
+        <Meaning
+          key={index}
+          antonyms={meaning.antonyms}
+          definitions={meaning.definitions}
+          partOfSpeech={meaning.partOfSpeech}
+          synonyms={meaning.synonyms}
+        />
+      ))}
+      {data?.sourceUrls && data?.sourceUrls.length > 0 && (
+        <div className="mt-6 border-t border-gray-200 py-5">
+          <div className="flex flex-wrap items-center">
+            <p className="text-xl">Source</p>
+            <Link
+              target="_blank"
+              href={data.sourceUrls[0]}
+              className="flex flex-wrap items-center text-fs-link text-gray-400 underline"
+            >
+              <span> {data.sourceUrls}</span>
+              <FaExternalLinkAlt className="text-sm" />
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
+	)
+}
